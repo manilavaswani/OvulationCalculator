d8e48b727680dd598039d3a33660125b14ff6def
manilavaswani <manilavaswani@gmail.com>
2025-02-19 14:38:06 +0530

    email reminders changes

--- src/app/reminder/reminder.component.ts
+++ src/app/reminder/reminder.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms'
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-reminder',
@@ -15,9 +16,9 @@ export class ReminderComponent {
 
   reminderdata: FormGroup;
   private firestore: Firestore = inject(Firestore);
+ 
 
-
-  constructor(private fb : FormBuilder){
+  constructor(private fb : FormBuilder, private http : HttpClient){
     this.reminderdata = this.fb.group(
       {
         email:['',Validators.required],
@@ -27,12 +28,14 @@ export class ReminderComponent {
     )
   }
 
+  //will get the value of emailfield so that we can validate that field
   get email() {
     return this.reminderdata.get('email');//email validation check
   }
 
+  //will get the value of lastperiod field so that we can validate that field
   get lastPeriod() {
-    return this.reminderdata.get('lastPeriod');
+    return this.reminderdata.get('lastPeriod'); //period date validation check
   }
 
   async onSubmit(){
@@ -43,7 +46,11 @@ export class ReminderComponent {
         try {
           const docRef = await addDoc(collection(this.firestore, 'periodTrackerData'), this.reminderdata.value);
           console.log("Document written with ID: ", docRef.id);
-          this.reminderdata.reset();
+          this.http.get('http://localhost:3000/trigger-reminder').subscribe({
+            next: (res: any) => console.log("✅ Script Triggered Successfully:", res),
+            error: (err: any) => console.error("❌ Error Triggering Script:", err)
+          });
+          this.reminderdata.reset();// reset the form value
         } catch (e) {
           console.error("Error adding document: ", e);
         }
